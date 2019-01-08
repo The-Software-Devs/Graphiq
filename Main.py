871f7c2753eacda92838f829b43fd1bdc3f70e35
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-08 14:03:36 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,66 +7,64 @@ import os
 from discord import Game
 import time
 
-client = discord.Client()
-client = commands.Bot(command_prefix = ".")
-client.remove_command("help")
-Client = discord.client
+bot = commands.Bot(command_prefix = ".")
+bot.remove_command('help')
 
-@client.event
+@bot.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="163 servers | 591,925 users",
+    await bot.change_presence(game=discord.Game(name="163 servers | 591,925 users",
 url="https://twitch.tv/celabrat", type=1))
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
+@bot.event
 async def on_message_edit(before, after):
     fmt = '**{0.author}** edited their message: :arrow_up: \n{1.content}'
-    await client.send_message(after.channel, fmt.format(after, before))
+    await bot.send_message(after.channel, fmt.format(after, before))
 
-@client.event
+@bot.event
 async def on_message(message):
     # we do not want the bot to reply to itself
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await bot.send_message(message.channel, msg)
 
     if message.content.startswith('.welcome'):
         msg = 'Thanks for having me in your server {0.author.mention}!'.format(message)
-        await client.send_message(message.channel, msg)
+        await bot.send_message(message.channel, msg)
 
     if message.content.startswith('.ping'):
         userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+        await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
 
     if message.content.startswith("inviteme"):
         userID = message.author.id
         msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format((message))
-        await client.send_message(message.channel, msg)
+        await bot.send_message(message.channel, msg)
 
     if message.content.startswith('.adminme'):
         userID = message.author.id
-        await client.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userID))
+        await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userID))
     
     if message.content.startswith('.login'):
         if message.author.id == "341933833136111617": #Replace <User ID> with the ID of the user you want to be able to execute this command!
             args = message.content.split(" ")
-            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+            await bot.send_message(message.channel, "%s" % (" ".join(args[1:])))
         else:
-            await client.send_message(message.channel, "You do not have permission to log into this service!")
+            await bot.send_message(message.channel, "You do not have permission to log into this service!")
     
     if message.content.startswith('.amiadmin'):
         if "<Role ID>" in [role.id for role in message.author.roles]: #Replace <Role ID> with the ID of the role you want to be able to execute this command
-            await client.send_message(message.channel, "You are an administrator!")
+            await bot.send_message(message.channel, "You are an administrator!")
         else:
-            await client.send_message(message.channel, "You are not an administrator!")
+            await bot.send_message(message.channel, "You are not an administrator!")
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def help(ctx):
 	server = ctx.message.server
 	author = ctx.message.author
@@ -76,6 +74,6 @@ async def help(ctx):
 	embed.add_field(name="hello", value="the bot will say hello to you")
 	embed.set_thumbnail(url=server.icon_url)
 	embed.set_footer(text="Requested by: " + author.name)
-	await client.say(embed=embed)
+	await bot.say(embed=embed)
     
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
