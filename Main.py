9f2b49b97bf256c646025b06c54f8090f72eddaa
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-08 15:10:42 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,30 +21,28 @@ url="https://twitch.tv/celabrat", type=1))
 
 @bot.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await bot.send_message(message.channel, msg)
-
-    if message.content.startswith('.welcome'):
-        msg = 'Thanks for having me in your server {0.author.mention}!'.format(message)
-        await bot.send_message(message.channel, msg)
-
-    if message.content.startswith('.ping'):
-        userID = message.author.id
-        await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
-
-    if message.content.startswith("inviteme"):
-        userID = message.author.id
-        msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format((message))
-        await bot.send_message(message.channel, msg)
-
-    if message.content.startswith('.adminme'):
-        userID = message.author.id
-        await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userID)
+	if message.author == bot.user:
+		return
+	
+	if message.content.startswith('hello'):
+		msg = 'Hello {0.author.mention}'.format(message)
+		await bot.send_message(message.channel, msg)
+	
+	if message.content.startswith('.welcome'):
+		msg = 'Thanks for having me in your server {0.author.mention}!'.format(message)
+		await bot.send_message(message.channel, msg)
+	
+	if message.content.startswith('.ping'):
+		userID = message.author.id
+		await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
+		
+	if message.content.startswith("inviteme"):
+		userID = message.author.id
+		msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format((message))
+		await bot.send_message(message.channel, msg)
+	if message.content.startswith('.adminme'):
+		userID = message.author.id
+		await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userID
 	await bot.process_commands(message)
 
 @bot.command(pass_context=True)
