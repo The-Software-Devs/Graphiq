57497818ce9ae7e974a231270e4cb67d03b29c8d
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-08 15:46:51 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import random
 import os
-from discord import Game
 import time
 
 bot = commands.Bot(command_prefix = ".")
@@ -23,21 +21,17 @@ url="https://twitch.tv/celabrat", type=1))
 async def on_message(message):
 	if message.content.startswith('hello'):
 		msg = 'Hello {0.author.mention}'.format(message)
-	await bot.send_message(message.channel, msg)
-	
+		await bot.send_message(message.channel, msg)
 	if message.content.startswith('.welcome'):
 		msg = 'Thanks for having me in your server {0.author.mention}!'.format(message)
-	await bot.send_message(message.channel, msg)
-	
+		await bot.send_message(message.channel, msg)
 	if message.content.startswith('.ping'):
 		userID = message.author.id
-	await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
-		
+		await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
 	if message.content.startswith("inviteme"):
 		userID = message.author.id
 		msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format((message))
-	await bot.send_message(message.channel, msg)
-	
+		await bot.send_message(message.channel, msg)
 	if message.content.startswith('.adminme'):
 		userID = message.author.id
 		await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userI
@@ -45,14 +39,14 @@ async def on_message(message):
 
 @bot.command(pass_context=True)
 async def help(ctx):
-		server = ctx.message.server
-		author = ctx.message.author
-		embed = discord.Embed(title="Help is here!", description=" ", color=0xFFFF)
-		embed.add_field(name="inviteme", value="gives you bot invite link")
-		embed.add_field(name=".ping", value="the bot ping you")
-		embed.add_field(name="hello", value="the bot will say hello to you")
-		embed.set_thumbnail(url=server.icon_url)
-		embed.set_footer(text="Requested by: " + author.name)
-		await bot.say(embed=embed)
+        server = ctx.message.server
+	author = ctx.message.author
+	embed = discord.Embed(title="Help is here!", description=" ", color=0xFFFF)
+	embed.add_field(name="inviteme", value="gives you bot invite link")
+	embed.add_field(name=".ping", value="the bot ping you")
+	embed.add_field(name="hello", value="the bot will say hello to you")
+	embed.set_thumbnail(url=server.icon_url)
+	embed.set_footer(text="Requested by: " + author.name)
+	await bot.say(embed=embed)
     
 bot.run(os.environ['BOT_TOKEN'])
