5ce530e27e905a0cc7fb54c765cf6b1e30bf24f8
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-01-11 16:05:42 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -54,6 +54,10 @@ async def help(ctx):
 	embed.add_field(name=".idea", value="Send your idea about new commands the bot don't have.")
 	embed.add_field(name=".bothelp", value="Bot help within server.")
 	await bot.say(embed=embed)
+	channel = bot.get_channel('532949494036168706')
+	embed = discord.Embed(title=f"User: {ctx.message.author.name} have used help command", description=f"ID: {ctx.message.author.id}", color=0xff9393)
+	await bot.send_message(channel, embed=embed)
+
 	
 @bot.command(pass_context=True)
 async def bug(ctx, *, reportmsg: str):
