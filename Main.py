6b5c5b92deb4b78da1efaea30964399ab11d7fd0
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-12 12:04:07 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -192,7 +192,7 @@ async def _eval(ctx, *, command):
     if inspect.isawaitable(res):
         await bot.say(await res)
     else:
+	await bot.say(res)
 	await bot.delete_message(ctx.message)
-    	await bot.say(res)
 		      
 bot.run(os.environ['BOT_TOKEN'])
