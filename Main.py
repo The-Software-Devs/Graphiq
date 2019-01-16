15c39d2a32c4caad75ed9ac6fb38b7db743ebd91
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-01-16 18:25:41 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -38,6 +38,7 @@ async def on_message(message):
 		await asyncio.sleep(5)
 		await bot.send_message(message.channel, ":arrows_counterclockwise: Restarting Bot...".format(message))
 		await asyncio.sleep(5)
+		  await bot.delete_message(ctx.message)
 		await bot.send_message(message.channel, ":white_check_mark: Bot Successfully Restarted".format(message))
 		
 def user_is_me(ctx):
