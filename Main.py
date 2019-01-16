0976e4be7231252d622c4e003c06a819d332baa7
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-01-16 16:34:03 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -36,8 +36,8 @@ async def on_message(message):
 	
 	
 	@bot.command(pass_context=True, no_pm=True)
-	await bot.delete_message(ctx.message)
 	if message.content.startswith('.restart'):
+	await bot.delete_message(ctx.message)
 async def help(ctx):
 	author = ctx.message.author
 	embed = discord.Embed(title="Bot Restart", description=" ", color=0xFFFF)
