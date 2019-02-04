afd3cd068d3bf0928eb95390a381d2c5b168faea
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-02-04 22:29:28 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -329,8 +329,8 @@ async def broadcast(ctx, *, msg):
         pass
 		
 @bot.command(pass_context=True)
-if ctx.message.author.id == "341933833136111617":
 async def leave(ctx):
+if ctx.message.author.id == "341933833136111617":
         server = ctx.message.server
         await bot.say(f'Are you sure you want Graphiq to leave {ctx.message.server} to confirm type `yes`')
         await bot.wait_for_message(author=ctx.message.author, content=['yes','y','Yes','YES','Y'])
