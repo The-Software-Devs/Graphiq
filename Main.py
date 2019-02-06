b9dcd97cc0c9acdae4e2a4e2e4dcc8476ff7d204
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-02-06 20:29:11 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -291,12 +291,12 @@ async def _eval(ctx, *, command):
             await bot.say(await res)
         else:
             await bot.delete_message(ctx.message)
-            await asyncio.sleep(15)
             await bot.send_typing(ctx.message.channel)
+            await asyncio.sleep(2)
             await bot.say(res)
     else:
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
         await bot.send_message(ctx.message.channel, "Sorry {} You have no permission to use this command only the bot owners can use this.".format(ctx.message.author.mention))
 	
 	
