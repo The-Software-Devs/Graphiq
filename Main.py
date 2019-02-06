21d105cb951c72ef5ff8f8e0c533e2f6d571d731
Graphiqs <46428139+Graphiqs@users.noreply.github.com>
2019-02-06 21:15:00 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ async def on_message(message):
 @bot.event
 async def on_message(message):
 	if message.content.startswith('.credits'):
-		embed=discord.Embed(description=f"Bot Owner & Founder: Mxchael & RJ_RBLX05		| Vice Chairman: JayHaggs		 | Bot Developers: NoobPerson, Tunyo-Tex. {message.author.mention}")
+		embed=discord.Embed(description=f"Bot Owner & Founder: Mxchael & RJ_RBLX05		| Vice Chairman: JayHaggs		 | Bot Developers: NoobPerson, Tunyo-Tex. Thank you for using my bot {message.author.mention}")
 		embed.set_image(url="https://cdn.discordapp.com/attachments/524655977832775710/541446963887996939/Fade_image.png")    
 		await bot.send_message(message.channel, embed=embed)
 	await bot.process_commands(message)
