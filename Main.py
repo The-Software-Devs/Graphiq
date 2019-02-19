29dd701a507a79c4f38fbeadd627ba9164debd85
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-02-19 22:26:25 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -717,7 +717,7 @@ async def broadcast(ctx, *, msg):
 		
 @bot.command(name='eval', pass_context=True)
 async def _eval(ctx, *, command):
-    if ctx.message.author.id == "493075860975386646" or "341933833136111617" or "305093302561144833":
+    if ctx.message.author.id == "493075860975386646" or "341933833136111617" or "305093302561144833" or "457214181268127747":
         res = eval(command)
         if inspect.isawaitable(res):
             await bot.say(await res)
