98d50885f2f1e92500c40717c74ca0f477de4f15
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-02-21 15:04:35 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,8 +8,6 @@ import os
 import random
 import json
 import datetime
-from bot.commands.command import Command
-from bot.utilities.permission import Permission
 
 bot = commands.Bot(command_prefix = "b.")
 bot.remove_command('help')
@@ -73,34 +71,6 @@ async def on_ready():
     print("Change status for {} is ready!".format(bot.user.name))
 
 
-
-class Sleep(Command):
-    """Allows admins and trusted mods to pause the bot."""
-
-    perm = Permission.Moderator
-
-    def __init__(self, bot):
-        """Initialize variables."""
-        self.responses = {}
-
-    def match(self, bot, user, msg, tag_info):
-        """Match if message is !sleep or !wakeup."""
-        cmd = msg.lower().strip()
-
-        if (user in bot.trusted_mods or bot.get_permission(user) == 3):
-            return cmd.startswith("!sleep") or cmd.startswith("!wakeup")
-
-    def run(self, bot, user, msg, tag_info):
-        """Put the bot to sleep or wake it up."""
-        self.responses = bot.responses["Sleep"]
-        cmd = msg.lower().replace(' ', '')
-        if cmd.startswith("!sleep"):
-            bot.write(self.responses["bot_deactivate"]["msg"])
-            bot.close_commands()
-            bot.pause = True
-        elif cmd.startswith("!wakeup"):
-            bot.write(self.responses["bot_activate"]["msg"])
-bot.pause = False
 
 
 @bot.command(pass_context=True)
