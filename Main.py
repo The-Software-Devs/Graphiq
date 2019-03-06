f65df1d5f7eb78f1f52dfcaa20a89e5688cf165b
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-03-06 16:29:24 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -136,12 +136,13 @@ async def on_command_error(error, ctx):
 async def logout():
     m1=await bot.say("<a:warning:552881993445015553> This can get you a ban from using the bot and very dangerous. This can shut down the bot!")
     await bot.say('You have disregarded the warning and the bot is shutting down in...')
+    await bot.delete_message()
     await bot.edit_message(m1,new_content='<a:Graphiqloading:551796596350910475> 3')
-    asyncio.sleep(1)
+    asyncio.sleep(5)
     await bot.edit_message(m1,new_content='<a:Graphiqloading:551796596350910475> 2')
-    asyncio.sleep(1)
+    asyncio.sleep(5)
     await bot.edit_message(m1,new_content='<a:Graphiqloading:551796596350910475> 1')
-    await bot.edit_message(m1,new_content='<a:Graphiqloading:551796596350910475> 1')
+    asyncio.sleep(5)
     await bot.edit_message(m1,new_content='<a:tick:552885211122237440> Bot shutdown! <a:warning:552881993445015553>')
 
 	
