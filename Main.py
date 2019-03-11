646dbedc95cc61753828bf252f83a76578feb877
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-03-11 17:36:36 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,6 +13,10 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import urllib
 import urllib.request
+import youtube_dl
+import inspect
+from discord import opus
+import requests, bs4
 
 from discord.utils import get
 
@@ -67,6 +71,174 @@ async def picker():
 async def on_ready():
     bot.loop.create_task(picker())
     print("Change status for {} is ready!".format(bot.user.name))
+	
+	
+	
+# Music Script. THIS CANNOT BE EDITED OR COPIED!!!!
+
+start_time = time.time()
+
+bot = commands.Bot(command_prefix=("m."))
+songs = asyncio.Queue()
+play_next_song = asyncio.Event()
+client.remove_command("help")
+
+players = {}
+queues = {}
+
+def check_queue(id):
+	if queues[id] != []:
+		player = queues[id].pop(0)
+		players[id] = player
+		player.start()
+
+	
+async def audio_player_task():
+    while True:
+        play_next_song.clear()
+        current = await songs.get()
+        current.start()
+        await play_next_song.wait()
+
+
+def toggle_next():
+    client.loop.call_soon_threadsafe(play_next_song.set)
+
+
+@bot.command(pass_context=True)
+async def plays(ctx, url):
+	if not bot.is_voice_connected(ctx.message.server):
+		voice = await bot.join_voice_channel(ctx.message.author.voice_channel)
+	else:
+		voice = bot.voice_client_in(ctx.message.server)
+		
+		player = await voice.create_ytdl_player(url, after=toggle_next)
+		await songs.put(player)
+
+	
+
+    
+@bot.command(name="join", pass_context=True, no_pm=True)
+async def _join(ctx):
+    user = ctx.message.author
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully connected to voice channel:", value=channel)
+    await bot.say(embed=embed)
+	
+@bot.command(name="leave", pass_context=True, no_pm=True)
+async def _leave(ctx):
+    user = ctx.message.author
+    server = ctx.message.server
+    channel = ctx.message.author.voice.voice_channel
+    voice_client = bot.voice_client_in(server)
+    await voice_client.disconnect()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully disconnected from:", value=channel)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].pause()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Paused", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def skip(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].stop()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Skipped", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+	
+@bot.command(name="play", pass_context=True)
+async def _play(ctx, *, name):
+	author = ctx.message.author
+	name = ctx.message.content.replace("m.play ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_bot = client.voice_client_in(server)
+	player = await voice_bot.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	players[server.id] = player
+	print("User: {} From Server: {} is playing {}".format(author, server, title))
+	player.start()
+	embed = discord.Embed(description="**__Song Play By MUZICAL DOCTORB__**")
+	embed.set_thumbnail(url="https://i.pinimg.com/originals/03/2b/08/032b0870b9053a191b67dc8c3f340345.gif")
+	embed.add_field(name="Now Playing", value=title)
+	await bot.say(embed=embed)
+	
+@client.command(pass_context=True)
+async def queue(ctx, *, name):
+	name = ctx.message.content.replace("m.queue ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = bot.voice_client_in(server)
+	player = await voice_bot.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	
+	if server.id in queues:
+		queues[server.id].append(player)
+	else:
+		queues[server.id] = [player]
+	embed = discord.Embed(description="**__Song Play By MUZICAL DOCTORB__**")
+	embed.add_field(name="Video queued", value=title)
+	await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].resume()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Resumed", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+	
+	
+@client.command()
+async def invite():
+  	"""Bot Invite"""
+  	await client.say("📧Check DMs For Information✅")
+  	await client.whisper("Add me with this link {}".format(discord.utils.oauth_url(client.user.id)))	
+	
+	
+	
+@bot.command(pass_context=True)
+async def help_music(ctx):
+	user = ctx.message.author
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Music commands:", value="m.play | m.join | m.leave | m.pause | m.resume | m.skip | m.queue", inline=True)
+	embed.add_field(name="Credits:", value="m.credits")
+	embed.add_field(name="Other commands:", value="m.ping | m.support | m.stats | m.invite")
+	await bot.say(embed=embed)
+
+@bot.command(no_pm=True)
+async def credits():
+	"""credits who helped me"""
+	await bot.say('Imran helped me with this music bot')
+	
+bot.loop.create_task(audio_player_task())
+	
+	
 	
 @bot.command(pass_context=True)
 async def urban(ctx):
