0d26195fd46b9ebe67d9dbde297fd138caadd5e5
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-03-13 07:45:17 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -160,7 +160,7 @@ async def on_command_error(error, ctx):
 		draw.text((10, 70), "command you given!", (255, 255, 255), font=font)
 		draw.text((10, 140), "Are you sure this command exists?", (255, 255, 255), font=font)
 		draw.text((10, 210), "See if the commands exists b.help.", (255, 255, 255), font=font)
-		draw.text((10, 1100), "Graphiq. 2019	||	         Command Logged.", (255, 255, 255), font=font)
+		draw.text((10, 1100), "Graphiq. 2019	  	         Command Logged.", (255, 255, 255), font=font)
 		img.save('error.png') #Change infoimg2.png if needed.
 		await bot.send_file(ctx.message.channel,"error.png")
 	
