68be7c625ce04236775ca191f71be003c4279436
Graphiq 2019 <46428139+Graphiqs@users.noreply.github.com>
2019-03-17 22:50:02 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -97,7 +97,7 @@ async def on_member_remove(user):
     embed.add_field(name='<:member:556962083426795526> Avatar URL',value=user.avatar_url)
     embed.add_field(name='<:cog:553328079272017932> Top Role',value=user.top_role)
     embed.set_footer(icon_url=user.avatar_url,text=user.joined_at)
-    await bot.send_message(discord.Object(id='556904666714079237'),embed=embed)
+    await bot.send_message(discord.Object(id='556971586230550549'),embed=embed)
 
 @bot.event
 async def on_member_join(user):
@@ -110,7 +110,7 @@ async def on_member_join(user):
     embed.add_field(name='<:cog:553328079272017932> Top Role',value=user.top_role)
     embed.add_field(name='<:cog:553328079272017932> Server Name ',value=user.server.name)
     embed.set_footer(icon_url=user.avatar_url,text=user.joined_at)
-    await bot.send_message(discord.Object(id='556904666714079237'),embed=embed)
+    await bot.send_message(discord.Object(id='556971586230550549'),embed=embed)
 	
 @bot.command(pass_context=True)
 async def urban(ctx):
